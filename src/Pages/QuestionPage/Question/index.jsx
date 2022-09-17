34cf5c6c6e4d8e914dbb6264a1e0289c9eae7acd
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-09-17 12:18:07 -0300

    feat: add button finalizar

--- src/Pages/QuestionPage/Question/index.jsx
+++ src/Pages/QuestionPage/Question/index.jsx
@@ -3,45 +3,53 @@ import React from 'react';
 
 export default function Question(props) {
     return (
-        <li className={style.item}>
-            <div className={style.item__info}>
-                <span>{props.category}</span>
-                <span>Difficulty: {props.difficulty}</span>
-            </div>
-            <p className={style.item__question}>{props.id + 1} - {props.question}</p>
-            
-            <ul
-                className={style.item__answers}
-            >
-                {props.answers.sort().map((alternativa, index) =>
-                    <li className={style.answers} key={index}>
-                        <input value={alternativa} type="radio" name={`Question:${props.id}`} id={`${props.id}:${index}`} />
-                        <label htmlFor={`${props.id}:${index}`}>{alternativa}</label>
-                    </li>)}
-            </ul>
-            <button
-                onClick={(e) => {
-                 let ListaRespostas = e.target.previousSibling.childNodes;
-                 ListaRespostas.forEach(li =>{
-                   if(li.firstChild.checked){
-                        if(li.firstChild.value === props.correct_answer ){
-                           props.atualizaContador();
-                           e.target.textContent = 'Acertou';
-                        }else{
-                            e.target.textContent = 'Errou';
-                        }
-                        ListaRespostas.forEach(li =>{
-                            li.firstChild.disabled = true;
-                        })
-                   }
+      
 
-                 })
-                 
-                }}
-                className={style.confirmar}
+
+            <li className={style.item}>
+                <div className={style.item__info}>
+                    <span>{props.category}</span>
+                    <span>Difficulty: {props.difficulty}</span>
+                </div>
+                <p className={style.item__question}>{props.id + 1} - {props.question}</p>
+
+                <ul
+                    className={style.item__answers}
+                >
+                    {props.answers.sort().map((alternativa, index) =>
+                        <li className={style.answers} key={index}>
+                            <input value={alternativa} type="radio" name={`Question:${props.id}`} id={`${props.id}:${index}`} />
+                            <label htmlFor={`${props.id}:${index}`}>{alternativa}</label>
+                        </li>)}
+                </ul>
+                <button
+                    type='button'
+                    onClick={(e) => {
+                        let ListaRespostas = e.target.previousSibling.childNodes;
+                        ListaRespostas.forEach(li => {
+                            if (li.firstChild.checked) {
+                                if (li.firstChild.value === props.correct_answer) {
+                                    props.atualizaContador();
+                                    e.target.textContent = 'Acertou';
+
+                                } else {
+                                    e.target.textContent = 'Errou';
+
+                                }
+                                ListaRespostas.forEach(li => {
+                                    li.firstChild.disabled = true;
+                                })
+
+                            }
+
+                        })
+
+                    }}
+                    className={style.confirmar}
                 >Confirmar
-            </button>
-        </li>
+                </button>
+            </li>
+   
     )
 }
 
--- src/Pages/QuestionPage/index.jsx
+++ src/Pages/QuestionPage/index.jsx
@@ -1,21 +1,21 @@
 import Question from "./Question"
 import React, { useState, useEffect, useContext } from "react";
 import styles from "./QuestionPage.module.css"
-import {useParams}from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 
 export default function QuestionPage() {
     const [questions, setQuestions] = useState([]);
     const [numAcertos, setNumAcertos] = useState(0);
-    
-    function atualizaContador(){
+
+    function atualizaContador() {
         setNumAcertos(prevState => prevState + 1)
     }
-    function atualizaLista(){
+    function atualizaLista() {
 
     }
 
-    const {quantidade} = useParams();
+    const { quantidade } = useParams();
     useEffect(() => {
 
         async function recebeQuestoes() {
@@ -45,43 +45,44 @@ export default function QuestionPage() {
     }, []);
 
     return (
-        <section className={styles.questionPage}>
-            <div>
-                <div className={styles.taskContainer}>
+        <section >
+            <form action="submit" className={styles.questionPage}>
+               
+                <div>
+                    <div className={styles.taskContainer}>
 
-                    {questions.map((question, index) => (
-                        <Question
-                            key={index}
-                            id={index}
-                            category={question.category}
-                            difficulty={question.difficulty}
-                            question={question.question}
-                            answers={question.answers}
-                            correct_answer={question.correct_answer}
-                            incorrect_answers={question.incorrect_answers}
-                            atualizaContador = {atualizaContador}
-                        />
-                    ))
-                    }
-
-
+                        {questions.map((question, index) => (
+                            <Question
+                                key={index}
+                                id={index}
+                                category={question.category}
+                                difficulty={question.difficulty}
+                                question={question.question}
+                                answers={question.answers}
+                                correct_answer={question.correct_answer}
+                                incorrect_answers={question.incorrect_answers}
+                                atualizaContador={atualizaContador}
+                            />
+                        ))
+                        }
+                    </div>
+                    <button type="submit">Finalizar</button>
                 </div>
 
-            </div>
+                <div className={styles.infoContainer}>
+                    <ul className={styles.questionList}>
+                        {
+                            questions.map((element, index) => {
+                                return <li onClick={e => console.log(e)} key={index}><span>{index + 1}</span></li>
+                            })
+                        }
 
-            <div className={styles.infoContainer}>
-                <ul className={styles.questionList}>
-                    {
-                        questions.map((element, index) => {
-                            return <li onClick={e => console.log(e)} key={index}><span>{index + 1}</span></li>
-                        })
-                    }
-
-                </ul>
-                <div className={styles.relatorioContainer}>
-                    <span>Contador:<span >{numAcertos}</span> / {questions.length}</span>
+                    </ul>
+                    <div className={styles.relatorioContainer}>
+                        <span>Contador:<span >{numAcertos}</span> / {questions.length}</span>
+                    </div>
                 </div>
-            </div>
+            </form>
         </section>
     )
 }
